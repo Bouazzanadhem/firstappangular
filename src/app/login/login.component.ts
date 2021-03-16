636b2bd355d891d640a8ea2bf4e9154c924fb61c
Bouazzanadhem <bouazzanadhem@gmail.com>
2021-03-16 14:53:33 +0100

    commit

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 
 @Component({
@@ -8,26 +9,37 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  submitted = false;
   loginForm = new FormGroup({
-    number1: new FormControl(''),
-    number2: new FormControl(''),
+    email: new FormControl('',[Validators.required, Validators.email]),
+    password: new FormControl('',[Validators.required, Validators.minLength(8)])
   });
 
-  constructor() { }
-
+  constructor(private route: Router) { }
+  users:any[]=[]
   ngOnInit(): void {
     console.log("bonjour");
+    this.users=JSON.parse(localStorage.getItem("users") || '[]');
+    // console.log(this.users);
     
   }
+  login(){
+    this.submitted = true;
+    if (this.loginForm.invalid){
+      return;
+    }
+    // console.log(this.loginForm.value);
+    let user=this.users.find(x => x.email == this.loginForm.value.email && x.password == this.loginForm.value.password)
+    console.log(user);
+    if (user == undefined){
+      alert('verifier votre information')
+    }else{
+      localStorage.setItem("user-connected",JSON.stringify(user));
+    }
+    // this.route.navigate(['/register'])
+     this.route.navigateByUrl('/register')
 
-  emailFormControl = new FormControl('', [
-    Validators.required,
-    Validators.email,
-  ]);
-  passwordFormControl = new FormControl('',[
-    Validators.required,
-    Validators.minLength(8),
-  ])
- 
+    
+  }
   
 }
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormControlName, FormGroup, Validators } from '@angular/forms';
+import {  AbstractControlOptions, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+// import { MustMatch } from '@app/_helpers';
 
 @Component({
   selector: 'app-register',
@@ -7,41 +9,9 @@ import { FormBuilder, FormControl, FormControlName, FormGroup, Validators } from
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
-  // profileForm = new FormGroup({
-  //   number1: new FormControl(''),
-  //   number2: new FormControl(''),
-  // });
-  // registerForm = new FormGroup({
-  //   fname: new FormControl('',),
-  //   lname: new FormControl(''),
-  //   password: new FormControl(''),
-  // });
-  // constructor(
-  //   public formBuilder: FormBuilder
-  // ) {
-  //   this.registerForm = this.formBuilder.group({
-  //     fname: new FormControl('', Validators.compose([
-  //       Validators.required
-  //     ])),
-  //     lname: new FormControl('',Validators.compose([
-  //       Validators.required
-  //     ])),
-  //     email: new FormControl('',Validators.compose([
-  //       Validators.required,
-  //       Validators.email
-  //     ])),
-  //     password: new FormControl('',Validators.compose([
-  //       Validators.required,
-  //       Validators.minLength(6)
-  //     ])),
-  //     confirmpassword: new FormControl('',Validators.compose([
-  //       Validators.required,
-  //       Validators.minLength(6)
-  //     ]))
-  //   },{
-  //     // Validators: this.password.bind(this)
-  //   });
-  // }
+       
+  submitted= false;
+  // formOptions: AbstractControlOptions = { validators: MustMatch('password', 'confirmpassword') };
   registerForm:  FormGroup = new FormGroup({
     fname: new FormControl('',[Validators.required]),
     lname: new FormControl('',[Validators.required]),
@@ -50,21 +20,25 @@ export class RegisterComponent implements OnInit {
     password: new FormControl('',[Validators.required, Validators.minLength(8)]),
     confirmpassword: new FormControl('',[Validators.required, Validators.minLength(8)])
   });
-  constructor() {  }
+  // ,this.formOptions
+  constructor(public route: Router) {  }
   ngOnInit(): void {
   }
   register(){
+    this.submitted = true;
+    if (this.registerForm.invalid){
+      return;
+    }
+    // console.log(this.registerForm.value);
+    let users= this.registerForm.value;
+    let p =JSON.parse(localStorage.getItem("users") || '[]');
+    p.push(users);
+    localStorage.setItem("users",JSON.stringify(p));
+    this.route.navigate(['/app-login'])
     
   }
   // password(formGroup: FormGroup){
   //   const {value: password } =  formGroup.get('password');
     
   // }
-  // affichage(){
-    
-  //   let x=this.profileForm.get("number1")?.value;
-  //   let y=this.profileForm.controls["number2"].value;
-  //   console.log(x+y);
-  //   }
-
 }
